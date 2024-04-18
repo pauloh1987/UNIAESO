c5b75ef29657dc13e0c3e2f69d0413fdd5df794e
Paulo Henrique de Souza Marques <145568567+pauloh1987@users.noreply.github.com>
2024-04-17 22:21:10 -0300

    Questão 19

---
+++ csvatv19.java
@@ -0,0 +1,29 @@
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class csvatv19 {
+    public static void main(String[] args) {
+        String nomeArquivo = "funcionarios.csv";
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter(nomeArquivo, true))) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite os detalhes do funcionário:");
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+            System.out.print("Cargo: ");
+            String cargo = scanner.nextLine();
+            System.out.print("Salário: ");
+            String salario = scanner.nextLine();
+
+            String[] linha = {nome, cargo, salario};
+            writer.writeNext(linha);
+
+            System.out.println("Informações do funcionário adicionadas ao arquivo CSV.");
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever no arquivo CSV: " + e.getMessage());
+        }
+    }
+}
